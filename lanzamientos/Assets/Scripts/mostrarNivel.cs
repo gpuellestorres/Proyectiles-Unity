4f36983e25a5dacdfb6c0386348b0dbb0d614901
gpuellestorres <gpuellestorres@gmail.com>
2016-04-10 19:01:17 -0300

    Nombre de nivel en el fondo

---
+++ lanzamientos/Assets/Scripts/mostrarNivel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mostrarNivel : MonoBehaviour {
+
+    Text Texto;
+
+	// Use this for initialization
+	void Start () {
+        Texto = FindObjectOfType<Text>();
+        string escena= SceneManager.GetActiveScene().name;
+
+        if (escena != "Menu")
+        {
+            Texto.text = escena;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
